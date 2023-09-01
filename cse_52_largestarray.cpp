0767b5fdef5a112db920c9904b5d0f98892060f9
Saalim-31 <134718581+Saalim-31@users.noreply.github.com>
2023-09-01 15:33:08 +0530

    Add files via upload

---
+++ cse_52_largestarray.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int num[5];
+	int i;
+	
+	for (i=0;i<=4;i++)
+	{
+	printf("Enter number %d of the array:-  %d");
+	scanf("%d",num[i]);
+}
+		for (i=0;i<=4;i++)
+		large=[0];
+		{
+			if(large<num[i])
+			Large=num[i];}
+			printf("Largest among the given numbers is :-	 %d",large);
+		}
+		return 0;
+	}
