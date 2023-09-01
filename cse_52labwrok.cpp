2319d535175d37cd0b6b396695a80ade9e42deeb
Saalim-31 <134718581+Saalim-31@users.noreply.github.com>
2023-09-01 15:17:30 +0530

    Add files via upload

---
+++ cse_52labwrok.cpp
@@ -0,0 +1,19 @@
+//PROGRAM TO TAKE 10 INPUT FOr USER AND DISPLAY THEM USING ARRAY FUNCTION
+#include<stdio.h>
+int main()
+{
+	float num[10];
+{
+	int i;
+	for (i=0;i<10;i++)
+	printf("Enter number %d of the array:-  %d");
+	scanf("%f",num[i]);
+	}
+	{
+		int i;
+		for (i=0;i<10;i++)
+		printf("Element %d of the array is :-  %f",i,num[i]);
+	}
+	return 0;
+}
+
